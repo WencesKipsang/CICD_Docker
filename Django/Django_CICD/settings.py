045c011da50210f46e00c14132e76c6559cb160b
wences <cheruiyotwences@gmail.com>
2024-09-18 00:18:48 +0300

    updated

--- Django/Django_CICD/settings.py
+++ Django/Django_CICD/settings.py
@@ -77,6 +77,7 @@ DATABASES = {
         "PASSWORD": os.getenv('SQL_PASSWORD'),
         "HOST": os.getenv('SQL_HOST'),
         "PORT": os.getenv('SQL_PORT'),
+        
 
         # "ENGINE": os.getenv("SQL_ENGINE", "django.db.backends.sqlite3"),
         # "NAME": os.getenv("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
