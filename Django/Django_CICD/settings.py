5c90d5a7f5100d46143230bff683b44a83121599
wences <cheruiyotwences@gmail.com>
2024-09-29 16:11:00 +0300

    updated django-app images

--- Django/Django_CICD/settings.py
+++ Django/Django_CICD/settings.py
@@ -133,6 +133,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'staticfiles'),
 )
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'staticfiles'),
+# )
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
